49c4a79317d6e1bf545c7008f4f319324a017d52
rita-tamer <rb2200191@tkh.edu.eg>
2024-04-23 01:07:44 +0200

    Update Crypto.h

--- Crypto.h
+++ Crypto.h
@@ -8,10 +8,12 @@ public:
     void init() {
         // Initialize cryptographic library for later use
     }
-    std::string aesEncrypt(const unsigned char* input, size_t length) { //encrypts the data using AES converting plaintext into ciphertext through a calculate key based on the value of the length of the input
+    //encrypts the data using AES converting plaintext into ciphertext through a calculate key based on the value of the length of the input
+    std::string aesEncrypt(const unsigned char* input, size_t length) { 
         return std::string(reinterpret_cast<const char*>(input), length);
     }
-    std::string aesDecrypt(const unsigned char* input, size_t length) { //decrypts the data using AES back into plaintext  through a calculated key based on the value of the length of the message
+     //decrypts the data using AES back into plaintext  through a calculated key based on the value of the length of the message
+    std::string aesDecrypt(const unsigned char* input, size_t length) {
         return std::string(reinterpret_cast<const char*>(input), length);
     }
 };
